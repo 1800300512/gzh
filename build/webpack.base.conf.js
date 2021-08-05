4e75b51177a6c3f51a967e55ae66f056e7361890
180300512 <212697301@qq,com>
2021-08-05 18:47:10 +0800

    gzh

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -15,6 +15,9 @@ module.exports = {
   entry: {
     app: './src/main.js'
   },
+  devServer: {
+    disableHostCheck:true
+  },
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
---
+++ src/test/java/com/example/gzh/GzhApplicationTests_1.java
@@ -0,0 +1,13 @@
+package com.example.gzh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GzhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
